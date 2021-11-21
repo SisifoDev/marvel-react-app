5a44bf3e3f2375fffd3e9471b52eeef1be130094
Manuel Perdomo <manuelperdomo1710@icloud.com>
2021-11-21 13:56:04 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const url = process.env.REACT_APP_URL;
+
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${url}`).then((res) => {
+      setCharacters(res.data.data.results);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {characters.map((character) => (
+        <div className="card" key={character.id}>
+          <h1>{character.title}</h1>
+          <h2>{character.modified}</h2>
+          <img
+            src={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+            alt={character.title}
+          />
+        </div>
+      ))}
     </div>
   );
 }
