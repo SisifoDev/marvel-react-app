1b4a11c23c510daaff2e7d837ec2ceb89f5b2518
Manuel Perdomo <manuelperdomo1710@icloud.com>
2021-12-05 16:03:49 -0500

    Grid

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 import { Card } from "./components/Card";
+import styled from "styled-components";
 
 function App() {
   const url = process.env.REACT_APP_URL;
@@ -16,16 +17,32 @@ function App() {
 
   return (
     <div className="App">
-      {characters.map((character) => (
-        <Card
-          key={character.id}
-          title={character.title}
-          date={character.modified}
-          img={`${character.thumbnail.path}.${character.thumbnail.extension}`}
-        />
-      ))}
+      <Title>Marvel Characters</Title>
+      <CharactersContainer>
+        {characters.map((character) => (
+          <Card
+            key={character.id}
+            title={character.title}
+            date={character.modified}
+            img={`${character.thumbnail.path}.${character.thumbnail.extension}`}
+          />
+        ))}
+      </CharactersContainer>
     </div>
   );
 }
+
+const Title = styled.h1`
+  font-size: 3rem;
+`;
+
+const CharactersContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  max-width: 1200px;
+  gap: 20px;
+  margin: 0 auto;
+`;
 
 export default App;
--- src/components/Card.js
+++ src/components/Card.js
@@ -20,16 +20,45 @@ const CardContainer = styled.div`
   /* left: 50%; */
   width: 320px;
   height: 430px;
+
   /* transform: translate(-50%, -50%); */
   background: url(${(props) => props.img}) center / cover;
   box-shadow: 0 7px 14px rgba(0, 0, 0, 0.3);
   border-radius: 8px;
 `;
 
-const CardOverlay = styled.div``;
+const CardOverlay = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  height: 100%;
+  background: rgb(0, 0, 0);
+  background: linear-gradient(
+    0deg,
+    rgba(0, 0, 0, 0.9) 0%,
+    rgba(0, 0, 0, 0.5) 40%,
+    rgba(151, 152, 152, 0.2) 100%
+  );
+  border-radius: 8px;
+`;
 
-const CardContent = styled.div``;
+const CardContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+`;
 
-const CardTitle = styled.p``;
+const CardTitle = styled.p`
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: coral;
+  margin: 0;
+`;
 
-const CardDescription = styled.p``;
+const CardDescription = styled.p`
+  font-size: 14px;
+  margin: 0;
+  color: aliceblue;
+`;
